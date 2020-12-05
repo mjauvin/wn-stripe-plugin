bd498954985ca23a664378313b39dde44723087f
Marc Jauvin <marc.jauvin@gmail.com>
2020-12-05 06:59:34 -0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 
 App::before(function ($request) {
     Route::any('/stripe/checkout/session/completed', function () {
-        $checkoutResult = \Request::input();
-        \Event::fire('studioazura.stripe.payment.completed', [$checkoutResult]);
+        $checkoutResult = Request::input();
+        Event::fire('studioazura.stripe.payment.completed', [$checkoutResult]);
     })->middleware('web'); 
 });
